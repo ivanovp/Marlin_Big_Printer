2f7324ce45c69a1c77ecbdae550b7652f96a616c
Peter Ivanov <ivanovp@gmail.com>
2018-09-29 07:27:17 +0200

    Configuration of temperature sensor was fixed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -334,8 +334,8 @@
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
-#define TEMP_SENSOR_5 11
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_5 0
+#define TEMP_SENSOR_BED 11
 #define TEMP_SENSOR_CHAMBER 0
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
