4d2dd57e0d7fa1ad373319ab9768c0c08fe24d86
Peter Ivanov <ivanovp@gmail.com>
2019-03-20 18:24:11 +0100

    PID was tuned.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -429,7 +429,7 @@
 // Comment the following line to disable PID and enable bang-bang.
 #define PIDTEMP
 #define BANG_MAX 255     // Limits current to nozzle while in bang-bang mode; 255=full current
-#define PID_MAX BANG_MAX // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
+#define PID_MAX 200 //BANG_MAX // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 #if ENABLED(PIDTEMP)
   #define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
@@ -445,9 +445,14 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Peter's Big Printer with single E3D v5
-  #define DEFAULT_Kp 11.41
-  #define DEFAULT_Ki 1.00
-  #define DEFAULT_Kd 32.44
+  //#define DEFAULT_Kp 11.41
+  //#define DEFAULT_Ki 1.00
+  //#define DEFAULT_Kd 32.44
+
+  // Peter's Big Printer with dual head
+  #define DEFAULT_Kp 8.84
+  #define DEFAULT_Ki 0.60
+  #define DEFAULT_Kd 32.74
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
